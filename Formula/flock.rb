8bfce46b9be5b16d2c11b6fc085acf790306b620
Qasim Iqbal <me@qas.im>
2022-12-30 00:01:23 -0500

    Initial formula

---
+++ Formula/flock.rb
@@ -0,0 +1,17 @@
+class Flock < Formula
+    desc "Rapid file download using concurrent connections"
+    homepage "https://github.com/qasim/Flock"
+    url "https://github.com/qaim/Flock.git", tag: "0.1.0", revision: "8b6ccf3be606cc08fbcc7290d61a83946e7acc91"
+    license "MIT"
+    head "https://github.com/qasim/Flock.git", branch: "main"
+
+    depends_on xcode: :build
+    depends_on :macos
+    depends_on macos: :monterey
+    uses_from_macos "swift"
+
+    def install
+        system "swift", "build", "-c", "release"
+        bin.install ".build/release/flock"
+    end
+end
