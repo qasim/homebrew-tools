cabf318b8baa42385964d0f21ab6c42104b7e841
Qasim Iqbal <me@qas.im>
2022-12-30 14:24:16 -0500

    Update to 0.2.0

--- Formula/flock.rb
+++ Formula/flock.rb
@@ -1,7 +1,7 @@
 class Flock < Formula
   desc "Rapid file download using concurrent connections"
   homepage "https://github.com/qasim/Flock"
-  url "https://github.com/qasim/Flock.git", tag: "0.1.1", revision: "885c9fbbdabf6d508f86cddc38d8ed864ddf703d"
+  url "https://github.com/qasim/Flock.git", tag: "0.2.0", revision: "67eca532965bb66eeec146ab803f07a78b02fc2f"
   license "MIT"
   head "https://github.com/qasim/Flock.git", branch: "main"
 
