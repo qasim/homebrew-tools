916b69509d83550313c01a0aeaf231f4603f8d17
Qasim Iqbal <me@qas.im>
2022-12-30 12:13:29 -0500

    Update to 0.1.1

--- Formula/flock.rb
+++ Formula/flock.rb
@@ -1,19 +1,19 @@
 class Flock < Formula
-    desc "Rapid file download using concurrent connections"
-    homepage "https://github.com/qasim/Flock"
-    url "https://github.com/qasim/Flock.git", tag: "0.1.0", revision: "8b6ccf3be606cc08fbcc7290d61a83946e7acc91"
-    license "MIT"
-    head "https://github.com/qasim/Flock.git", branch: "main"
+  desc "Rapid file download using concurrent connections"
+  homepage "https://github.com/qasim/Flock"
+  url "https://github.com/qasim/Flock.git", tag: "0.1.1", revision: "885c9fbbdabf6d508f86cddc38d8ed864ddf703d"
+  license "MIT"
+  head "https://github.com/qasim/Flock.git", branch: "main"
 
-    depends_on xcode: :build
-    depends_on :macos
-    depends_on macos: :monterey
-    uses_from_macos "swift"
+  depends_on xcode: :build
+  depends_on :macos
+  depends_on macos: :monterey
+  uses_from_macos "swift"
 
-    def install
-        chdir "CLI" do
-            system "swift", "build", "--disable-sandbox", "-c", "release"
-            bin.install ".build/release/flock"
-        end
+  def install
+    chdir "CLI" do
+      system "swift", "build", "--disable-sandbox", "-c", "release"
+      bin.install ".build/release/flock"
     end
+  end
 end
